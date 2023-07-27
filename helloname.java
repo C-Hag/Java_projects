362872cfdc22dd9a3e80b75327bebfe957d1a78a
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-07-27 21:53:11 +0200

    name classes to your file.

--- helloname.java
+++ helloname.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-class Main {
+class helloname {
   public static void main(String[] args) {
    Scanner scanner = new Scanner(System.in); 
     System.out.println("Please enter your name: ");
