6381dfe38542fe76973ea4e13226d757cef2f830
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-07-27 22:30:44 +0200

    date

--- structure/Main.java
+++ structure/Main.java
@@ -1,9 +1,10 @@
 package structure;
 
+import java.time.*;
+
 public class Main {
     public static void (String)[] args) {
         Account account1 = new Account();
-
-    }
-    
+        LocalDate ld = LocalDate.now();
+    } 
 }
