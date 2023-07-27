2c34494668fdf1378536ef3317abbbfe9d5e9336
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-07-27 22:25:13 +0200

    package & structures

---
+++ structure/Account.java
@@ -0,0 +1,30 @@
+package structure;
+
+public class Account {
+    String owner;
+    double balance; 
+    double minimumBalance;
+
+    public void deposit(double amount) {
+        // implementation
+    }
+
+    public void withdraw(double amount) {
+        // implementation
+
+    public double calculateCreditWorthiness() {
+        // implementation
+        return 100;
+
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+    
+    }
+}
