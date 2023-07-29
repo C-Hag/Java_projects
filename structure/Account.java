0e36c0faa57d2beecdbecdbc2fa31b510fb32b66
C-Hag <83981737+C-Hag@users.noreply.github.com>
2023-07-29 11:27:56 +0200

    getters & setters

--- structure/Account.java
+++ structure/Account.java
@@ -1,30 +1,54 @@
 package structure;
 
 public class Account {
-    String owner;
-    double balance; 
-    double minimumBalance;
+   private String owner;
+   private double balance; 
+   private double minimumBalance;
 
     public void deposit(double amount) {
         // implementation
     }
 
-    public void withdraw(double amount) {
-        // implementation
-
-    public double calculateCreditWorthiness() {
-        // implementation
-        return 100;
-
-    }
-
+    /**
+     * @return String return the owner
+     */
     public String getOwner() {
         return owner;
     }
 
+    /**
+     * @param owner the owner to set
+     */
     public void setOwner(String owner) {
         this.owner = owner;
     }
-    
+
+    /**
+     * @return double return the balance
+     */
+    public double getBalance() {
+        return balance;
     }
+
+    /**
+     * @param balance the balance to set
+     */
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    /**
+     * @return double return the minimumBalance
+     */
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    /**
+     * @param minimumBalance the minimumBalance to set
+     */
+    public void setMinimumBalance(double minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
+
 }
